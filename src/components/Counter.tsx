37b2a260796a1cf8e07796c278f8c4d5a6bdc7da
dodo-ch <chren.dodo@gmail.com>
2023-08-30 20:09:27 +0200

    fix: props counter

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -2,9 +2,13 @@
 import {motion, useMotionValue, useTransform, animate} from "framer-motion";
 import React, { useEffect, useRef } from "react";
 
+type Props = {
+    from: number,
+    to: number,
+    duration?: number
+}
 
-
-export const Counter = ({ from, to, duration }) => {
+export const Counter = ({ from, to, duration }: Props) => {
     const count = useMotionValue(from);
     const rounded = useTransform(count, (latest) => Math.round(latest));
 
