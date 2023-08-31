2dd7e50fc688abb4c21dcb4e6b061d0fa416d2e3
dodo-ch <chren.dodo@gmail.com>
2023-08-31 23:24:05 +0200

    fixed: height

--- src/components/SoldItems/SoldItems.tsx
+++ src/components/SoldItems/SoldItems.tsx
@@ -9,7 +9,7 @@ const SoldItems = () => {
     const targetRef = useRef(null);
     const isOnScreen = useIsElementOnScreen(targetRef);
     return (
-        <section className="relative" style={{height: '45vh'}}>
+        <section className="relative" style={{height: '650px'}}>
             <div className="absolute inset-0">
                 <Image
                     src={img4}
