bff46c0a8818c4b335e9b55bfe72cc07739afee3
dodo-ch <chren.dodo@gmail.com>
2023-09-14 00:34:36 +0200

    feat: partners slider

--- src/components/PartnersBanner/PartnersBanner.tsx
+++ src/components/PartnersBanner/PartnersBanner.tsx
@@ -1,89 +1,69 @@
-'use client'
-import React from 'react';
-import atlantic from '../../images/logos/atlantic.png'
-import def_jam from '../../images/logos/def_jam.png'
-import empire  from '../../images/logos/empire.png'
-import everybody from '../../images/logos/everybody.png'
-import interscope from '../../images/logos/interscope.png'
-import universal from '../../images/logos/universal.png'
-import warner from '../../images/logos/warner.png'
-import {Image} from "next/dist/client/image-component";
-
-
-const partners = [
-    <Image
-        priority
-        src={atlantic}
-        alt="atlantic"
-        width={400}
-    />,
-
-    <Image
-        priority
-        src={def_jam}
-        alt="def_jam"
-        width={400}
-
-    />,
-
-    <Image
-        priority
-        src={empire}
-        alt="empire"
-        width={400}
-    />,
-
-    <Image
-        priority
-        src={everybody}
-        alt="everybody"
-        width={400}
-    />,
-
-    <Image
-        priority
-        src={interscope}
-        alt="interscope"
-        width={300}
-    />,
-
-    <Image
-        priority
-        src={universal}
-        alt="universal"
-        width={1000}
-    />,
-
-    <Image
-        priority
-        src={warner}
-        alt="warner"
-        width={1000}
-    />,
-
-
-
-
-];
+import React, { useEffect, useRef } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';// Import Swiper styles
+import atlantic from '../../images/logos/atlantic.png';
+import def_jam from '../../images/logos/def_jam.png';
+import empire from '../../images/logos/empire.png';
+import everybody from '../../images/logos/everybody.png';
+import interscope from '../../images/logos/interscope.png';
+import universal from '../../images/logos/universal.png';
+import warner from '../../images/logos/warner.png';
+import { Image } from 'next/dist/client/image-component';
+import { Autoplay} from 'swiper/modules';
 
 const PartnersBanner = () => {
+    const swiperElRef = useRef(null);
+
+    useEffect(() => {
+        const swiper = swiperElRef.current.swiper;
+
+        // Listen for Swiper events
+        swiper.on('slideChange', () => {
+            console.log('Slide changed');
+        });
+
+        swiper.on('progress', (progress) => {
+            console.log('Slide progress:', progress);
+        });
+    }, []);
+
     return (
-        <div className='my-24' style={{height: "5vh"}}>
-            <div className="flex flex-row justify-items-stretch items-stretch grow-1 overflow-hidden">
-                <div
-                    id="partners"
-                    className='flex flex-row items-center justify-between justify-items-stretch items-stretch grow-1 '
-                >
-                    {partners.map((partner, index) => (
-                        <div
-                            key={index}
-                            className="partner-items flex flex-row items-center justify-items-stretch grow-1  "
-                        >
-                            {partner}
-                        </div>
-                    ))}
-                </div>
-            </div>
+        <div className="my-24" style={{ height: '5vh' }}>
+            <Swiper
+                ref={swiperElRef}
+                spaceBetween={20}
+                slidesPerView={3}
+                navigation={false}
+                loop={true}
+                centeredSlides={true}
+                autoplay={{
+                    delay: 2000,
+                    disableOnInteraction: false,
+                }}
+                modules={[Autoplay]}
+            >
+                <SwiperSlide>
+                    <Image className='mx-auto' priority src={atlantic} alt="atlantic" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <Image  className='mx-auto' priority src={def_jam} alt="def_jam" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <Image  className='mx-auto' priority src={empire} alt="empire" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <Image  className='mx-auto' priority src={everybody} alt="everybody" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <Image  className='mx-auto' priority src={interscope} alt="interscope" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <Image  className='mx-auto' priority src={universal} alt="universal" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <Image  className='mx-auto' priority src={warner} alt="warner" />
+                </SwiperSlide>
+            </Swiper>
         </div>
     );
 };
