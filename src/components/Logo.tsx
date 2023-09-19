f995e07788d403c8d5fcd2052b09aa63d15e1826
dodo-ch <chren.dodo@gmail.com>
2023-09-19 17:29:27 +0200

    fix: logo, album card

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from '../images/logo.svg'
+import logo from '../images/logo.png'
 import {Image} from "next/dist/client/image-component";
 const Logo = () => {
     return (
