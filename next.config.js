1d4e0b068b27c9eff23380ebcbefde8353b869b3
dodo-ch <chren.dodo@gmail.com>
2023-09-25 00:23:54 +0200

    next config

--- next.config.js
+++ next.config.js
@@ -1,58 +1,16 @@
 const { version } = require('./package.json');
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    publicRuntimeConfig: {
-        version,
-    },
-    reactStrictMode: true,
-    swcMinify: true,
-    images: {
-        minimumCacheTTL: 60,
-        domains: [
-            `${process.env.S3_UPLOAD_BUCKET}.s3.amazonaws.com`,
-            `${process.env.S3_UPLOAD_BUCKET}.s3.${process.env.S3_UPLOAD_REGION}.amazonaws.com`,
-        ],
-    },
-    async rewrites() {
-        return [
+    output: "export",
+    images: { unoptimized: true,
+        remotePatterns: [
             {
-                source: '/admin/:any*',
-                destination: '/admin',
+                protocol: 'http',
+                hostname: 'spadegrowth.com',
+                port: '',
+                pathname: '/**',
             },
-        ];
-    },
-    async headers() {
-        return [
-            {
-                source: "/trap/:slug",
-                headers: [
-                    {
-                        key: "Cache-Control",
-                        value: "public,max-age=31536000,immutable",
-                    },
-                ],
-            },
-            {
-                source: "/flag/:slug",
-                headers: [
-                    {
-                        key: "Cache-Control",
-                        value: "public,max-age=31536000,immutable",
-                    },
-                ],
-            },
-            {
-                source: "/logo_black.png",
-                headers: [
-                    {
-                        key: "Cache-Control",
-                        value: "public,max-age=31536000,immutable",
-                    },
-                ],
-            },
-        ];
-    },
+        ], }
 }
 
 module.exports = nextConfig
